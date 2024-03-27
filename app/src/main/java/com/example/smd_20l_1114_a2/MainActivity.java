7e1a2fda3d961c86e1dc33c440f98060bf16791a
komalw2001 <komalwaseemm@gmail.com>
2024-03-27 15:44:18 +0500

    init

---
+++ app/src/main/java/com/example/smd_20l_1114_a2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.smd_20l_1114_a2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
